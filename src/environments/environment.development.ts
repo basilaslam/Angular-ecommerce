ce27d287538ddafc186309f55fad81fd63ab5879
Basil Aslam N P <64439076+basilaslam@users.noreply.github.com>
2023-10-14 14:25:32 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  URI: 'https://ecommerce-0whk.onrender.com/api/v1',
+  URI: 'https://floatstore-cpeitwtrka-an.a.run.app/api/v1',
   RAZORPAY_Id: 'rzp_test_m1A0RyAZPtmHGw',
 } as const;
